9cb33947b8e3eb073c88420e44ef68019164e4ae
sundelun <delun.alan.sun@gmail.com>
2025-04-01 16:31:31 -0400

    upload a README

---
+++ README.md
@@ -0,0 +1,2 @@
+# Competitive-Programming
+Record all kinds of common used templates and all kinds of "good" questions in competitive-programming. Feel free to explore!
--- main.py
+++ main.py
@@ -31,39 +31,9 @@ class FenwickTree:
             i -= i & -i
         return s
 """
- 
+# 0x0416DF152c5eB7d975eED0333098711ca2B0eE1E
 def solve():
-    n = int(input())
     s = str(input())
-    # TI -> TLI -> TILI -> TILTI if there is one pair that could be operate then we can have a solution!
-    # TILII cntT = 1, cntL = 1, cntI = 3
-    # ILLL -> ITLLL -> ITILLL -> ITITLLL -> ITITILLL -> ITITITLLL
-    # LLI -> LLTI -> LLITI -> LLTITI
-    cntT = cntL = cntI = 0
-    for ch in s:
-        if ch == "T": cntT += 1
-        elif ch == "L": cntL += 1
-        else: cntI += 1
-    if cntT == cntI == cntL:
-        print(0)
-        return
-    mx = max(cntT, cntI, cntL)
-    if cntT == mx: mxch = "T"
-    elif cntI == mx: mxch = "I"
-    else: mxch = "L"
-    if cntT + cntI + cntL - mx == 0:
-        print(-1)
-        return
-    ans = mx * 3 - n
-    print(ans)
-    for i in range(n - 1):
-        if s[i] != s[i + 1]:
-            if s[i] == mxch:
-                for _ in range(ans):
-                    print(i + 1)
-            else:
-                for j in range(ans):
-                    print(i + j + 1)
-            return
-for _ in range(int(input())):
-    solve()
+    print(10)
+#for _ in range(int(input())):
+solve()
