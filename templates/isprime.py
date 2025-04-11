a071aaec9f6dd3a1b93c5432bf2aabacdbc23761
sundelun <delun.alan.sun@gmail.com>
2025-04-11 01:19:13 -0400

    small fix on isprime template

--- templates/isprime.py
+++ templates/isprime.py
@@ -1,7 +1,7 @@
 #代码来自0x3f，灵茶山艾府
 MX = 32000
 is_prime = [True] * (MX + 1)
-for i in range(2, MX):
+for i in range(2, MX + 1):
     if is_prime[i]:
-        for j in range(i * i, MX, i):
+        for j in range(i * i, MX + 1, i):
             is_prime[j] = False
