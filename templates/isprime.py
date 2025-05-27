2916c27b42076a95f3337b2880cafba25a07843d
sundelun <delun.alan.sun@gmail.com>
2025-05-27 12:54:55 +0800

    upload more prime numbers fact

--- templates/isprime.py
+++ templates/isprime.py
@@ -5,3 +5,13 @@ for i in range(2, MX + 1):
     if is_prime[i]:
         for j in range(i * i, MX + 1, i):
             is_prime[j] = False
+
+import math
+# check if number n is prime helper-function
+def is_prime(n: int) -> bool:
+    for i in range(2, math.isqrt(n) + 1):
+        if n % i == 0:
+            return False
+    return n >= 2
+
+# if an integer m is not divisible by any prime numbers within math.isqrt(m), then m is prime number as well
