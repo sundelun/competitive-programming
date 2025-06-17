a5ffc68c5f570bb2136a64cefda8c5f2dc58b581
sundelun <delun.alan.sun@gmail.com>
2025-06-17 12:34:27 +0800

    update the new comb template

---
+++ templates/comb_0x3f.py
@@ -0,0 +1,19 @@
+MOD = 10 ** 9 + 7
+MX = 10 ** 5
+
+f = [0] * MX  # f[i] = i!
+f[0] = 1
+for i in range(1, MX):
+    f[i] = f[i - 1] * i % MOD
+
+inv_f = [0] * MX  # inv_f[i] = i!^-1
+inv_f[-1] = pow(f[-1], -1, MOD)
+for i in range(MX - 1, 0, -1):
+    inv_f[i - 1] = inv_f[i] * i % MOD
+
+def comb(n: int, m: int) -> int:
+    return f[n] * inv_f[m] * inv_f[n - m] % MOD
+
+"""
+example call: ans = comb(10, 5) same as 10C5
+"""
